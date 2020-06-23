645ede99663561d1386603b65859bdbe7297fefb
therexone <renerjake@gmail.com>
2020-06-24 04:03:37 +0530

    Add: new data to daemon

--- scripts/linux-dae-mon.py
+++ scripts/linux-dae-mon.py
@@ -8,13 +8,30 @@ import psutil
 import webbrowser
 import os
 
+
 def getDeviceInfo():
     data = {}
     battery = psutil.sensors_battery()
+    ram =  psutil.virtual_memory()
+    disk = psutil.disk_usage('/')
+    swap = psutil.swap_memory()
     data['user'] = psutil.users()[0].name
     data['cpu_freq'] = psutil.cpu_freq()
-    data['ram_usage'] = psutil.virtual_memory().percent
-    data['disk_usage'] = psutil.disk_usage('/').percent
+    data['ram_data'] = {
+        'percentage_used': ram.percent,
+        'total': ram.total,
+        'available': ram.available
+    }
+    data['disk_data'] = {
+        'percentage_used': disk.percent,
+        'total': disk.total,
+        'free': disk.free
+    }
+    data['swap_data'] = {
+        'percentage_used': swap.percent,
+        'total': swap.total,
+        'free': swap.free
+    }
     data['sensor_temperatures'] = psutil.sensors_temperatures(fahrenheit=False)
     data['battery_percentage'] = battery.percent
     data['plugged'] = battery.power_plugged
